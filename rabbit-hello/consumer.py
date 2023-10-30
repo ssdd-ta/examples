184d1e3052130b6cba8f0b1c37dca047da987a5b
Ana Rubio <Ana.Rubio@uclm.es>
2023-10-30 20:52:10 +0100

    add rabbitmq example

---
+++ rabbit-hello/consumer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# Extracted from: https://www.rabbitmq.com/tutorials/tutorial-one-python.html
+
+import pika
+
+
+def callback(ch, method, properties, body):
+    print("[x] Received: %r" % body.decode("UTF-8"))
+
+
+localhost = pika.ConnectionParameters('localhost')
+connection = pika.BlockingConnection(localhost)
+
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+channel.basic_consume(
+    queue='hello',
+    auto_ack=True,
+    on_message_callback=callback,
+)
+
+print("[*] Waiting for messages. press Ctrl+C to exit")
+channel.start_consuming()
---
+++ rabbit-hello/producer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Extracted from: https://www.rabbitmq.com/tutorials/tutorial-one-python.html
+
+import pika
+import time
+
+
+localhost = pika.ConnectionParameters(host='localhost')
+connection = pika.BlockingConnection(localhost)
+
+channel = connection.channel()
+channel.queue_declare(queue="hello")
+
+message = "Hello world! {}".format(time.time())
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=message)
+
+print("[x] Sent: ", message)
+connection.close()
