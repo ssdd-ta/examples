542029b3311a908a43ffd11727437a017a4f4959
Ana Rubio <Ana.Rubio@uclm.es>
2024-10-06 20:54:05 +0200

    socket.protobuf: update

--- sockets.protobuf/udp-client.py
+++ sockets.protobuf/udp-client.py
@@ -9,7 +9,7 @@ if len(sys.argv) < 2:
     exit()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-destination = (sys.argv[1], 2002)
+destination = (sys.argv[1], 1234)
 
 reading = sensor_pb2.Reading()
 reading.Id = 1
@@ -19,5 +19,6 @@ reading.unit = "kg/m3"
 
 data = reading.SerializeToString()
 print(data)
+
 sock.sendto(data, destination)
 sock.close()
--- sockets.protobuf/udp-server.py
+++ sockets.protobuf/udp-server.py
@@ -4,12 +4,12 @@ import socket
 from sensor_pb2 import Reading
 
 sock = socket.socket(type=socket.SOCK_DGRAM)
-sock.bind(('', 2002))
+sock.bind(('', 1234))
 reading = Reading()
 
 while True:
     data, address = sock.recvfrom(1024)
-    print(f"sensor: {address}, raw-data: {data}")
+    print(f"sensor: {address},\nraw-data:\n{data}")
 
     reading.ParseFromString(data)
     print("Sensor {0.Id} ({1}) value:{0.value:.2f} {0.unit}".format(
